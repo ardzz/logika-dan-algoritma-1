a441d6826ba021e6ee30b8153896d459ebe84b87
reky <me@reky.dev>
2022-09-10 02:13:33 +0700

    Add new line each `printf()`

--- static_biodata.c
+++ static_biodata.c
@@ -2,10 +2,10 @@
 
 int main(){
 
-    printf("Nama            : Naufal Reky Ardhana");
-    printf("Tanggal Lahir   : 7 April 2004");
-    printf("Alamat          : Kendal");
-    printf("Asal Sekolah    : SMA Negeri 1 Kendal");
+    printf("Nama            : Naufal Reky Ardhana\n");
+    printf("Tanggal Lahir   : 7 April 2004\n");
+    printf("Alamat          : Kendal\n");
+    printf("Asal Sekolah    : SMA Negeri 1 Kendal\n");
 
     return 0;
 }
