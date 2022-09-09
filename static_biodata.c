9a53e0311ace049d26136c967ad664b98fd0314a
reky <me@reky.dev>
2022-09-10 01:45:24 +0700

    Add `static_biodata.c`

---
+++ static_biodata.c
@@ -0,0 +1,11 @@
+#include "stdio.h"
+
+int main(){
+
+    printf("Nama            : Naufal Reky Ardhana");
+    printf("Tanggal Lahir   : 7 April 2004");
+    printf("Alamat          : Kendal");
+    printf("Asal Sekolah    : SMA Negeri 1 Kendal");
+
+    return 0;
+}
